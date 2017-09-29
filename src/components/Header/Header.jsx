0b70e5be64143aa52a9971238cb4ae098a38d38a
Alex <korzhlesha@gmail.com>
2017-09-29 22:40:53 +0300

    add bootstrap

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,12 @@ import './header.scss';
 
 const Header = () => {
     return (
-        <header className="header">Header test</header>
+        <header className="header">Header test
+            <div className="row">
+                <div className="col">asdas</div>
+                <div className="col">asdas</div>
+            </div>
+        </header>
     );
 };
 
