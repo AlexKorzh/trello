f9059608245074530373f4452fdfab1a1d74717c
Alex <korzhlesha@gmail.com>
2017-11-16 21:59:19 +0200

    save board file

--- client/src/actions/board.js
+++ client/src/actions/board.js
@@ -3,19 +3,6 @@ import authorization from '../utils/auth';
 import currentHost from '../utils/host';
 import axios from 'axios';
 
-<<<<<<< HEAD
-const fetchBoards = () => {
-    return dispatch => {
-        axios.post(
-            `${currentHost}/getAllBoards`,
-            null,
-            {headers: {authorization}}
-        ).then(response => {
-            console.log('F E T C H || RESPONSE', response);
-        });
-    }
-}
-=======
 const fetchBoards = () => dispatch => {
     axios.post(
         `${currentHost}/getAllBoards`, null,
@@ -26,6 +13,5 @@ const fetchBoards = () => dispatch => {
         dispatch(getUserBoards(boards));
     })
 };
->>>>>>> d9dfa0ca0789b0b29c2d85270544b5d185473a49
 
 export default fetchBoards;
